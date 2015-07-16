75ac5a9e37db9a7281e4d8c1d715971e82996a1b
Todd Mohney <toddmohney@gmail.com>
2015-07-16 06:23:58 -0400

    Consolidate db connection functions

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import Control.Monad.Logger    (runStderrLoggingT)
 import Database.Persist
 import Database.Persist.Postgresql
 import Database.Persist.TH
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 
 import Data.ByteString.Char8 (pack)
 import qualified Web.Scotty as S
@@ -16,18 +17,22 @@ import qualified Web.Scotty as S
 import Config
 import Model
 
-connPoolSize = 10
+connPoolSize = 4
 
-main = do
+runDb = liftIO . dbFunction
+
+dbFunction query = do
   connStr <- postgresConnectionString
-  runStderrLoggingT $ withPostgresqlPool (pack connStr) connPoolSize $ \pool -> liftIO $
-    flip runSqlPersistMPool pool $ runMigration migrateAll
+  runStderrLoggingT $
+    withPostgresqlPool (pack connStr) connPoolSize $
+      \pool -> liftIO $ runSqlPersistMPool query pool
 
-  S.scotty 3000 $ do
-    S.get "/" $ S.html "Hello World!"
-    S.get "/events" $ S.html "Here are all of your application's events"
-    S.get "/event/:eventId" $ S.html "Here's that application event you asked for"
-    S.get "/organizers/:organizerId/events" $ S.html "Here are all of the events created by this organizer"
-
-
+main :: IO ()
+main = S.scotty 3000 $ do
+  S.middleware logStdoutDev
+  runDb $ runMigration migrateAll
+  S.get "/" $ S.html "Hello World!"
+  S.get "/events" $ S.html "Here are all of your application's events"
+  S.get "/event/:eventId" $ S.html "Here's that application event you asked for"
+  S.get "/organizers/:organizerId/events" $ S.html "Here are all of the events created by this organizer"
 

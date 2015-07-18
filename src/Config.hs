fbe4f6004478f88eaa7514f25656f9445601de3b
Todd Mohney <toddmohney@gmail.com>
2015-07-18 19:47:36 -0400

    Extract configuration from Main module

--- src/Config.hs
+++ src/Config.hs
@@ -1,26 +1,119 @@
-module Config (postgresConnectionString) where
-  import Control.Applicative
-  import System.Environment (getEnv)
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-  type ConfigPair = (String, IO String)
+module Config where
+import Control.Applicative (Applicative)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Logger (runNoLoggingT, runStdoutLoggingT)
+import Control.Monad.Reader (MonadReader, ReaderT)
+import Data.Default (def)
+import Data.Text.Encoding (encodeUtf8)
+import qualified Data.Text as T
+import qualified Database.Persist as DB
+import qualified Database.Persist.Postgresql as DB
+import Network.Wai.Handler.Warp (Settings, defaultSettings, setFdCacheDuration, setPort)
+import System.Environment (lookupEnv)
+import Web.Heroku (parseDatabaseUrl)
+import Web.Scotty.Trans (Options, scottyOptsT, settings, verbose)
 
-  postgresConnectionString :: IO String
-  postgresConnectionString = foldr1 concatWithSpaceSeparator $ mergePairs '=' postgresConfig
-    where 
-      concatWithSpaceSeparator :: Applicative f => f String -> f String -> f String
-      concatWithSpaceSeparator x acc = (++) <$> (++ " ") <$> x <*> acc
+data Config = Config
+  { environment :: Environment
+  , pool :: DB.ConnectionPool
+  }
 
-  postgresConfig :: [ConfigPair]
-  postgresConfig = [ 
-                     ("host",    getEnv "EVENT_CALENDAR_DB_HOST")
-                   , ("dbname",  getEnv "EVENT_CALENDAR_DB_NAME")
-                   , ("user",    getEnv "EVENT_CALENDAR_DB_USER")
-                   , ("port",    getEnv "EVENT_CALENDAR_DB_PORT")
-                   ]
+data Environment
+  = Development
+  | Production
+  | Test
+  deriving (Eq, Read, Show)
 
-  mergePairs :: Char -> [ConfigPair] -> [IO String]
-  mergePairs sep [] = []
-  mergePairs sep xs = map (mergePair sep) xs
+newtype ConfigM a = ConfigM
+ { runConfigM :: ReaderT Config IO a
+ } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
 
-  mergePair :: Char -> ConfigPair -> IO String
-  mergePair sep pair = ((fst pair ++ [sep]) ++) <$> snd pair
+getConfig :: IO Config
+getConfig = do
+  e <- getEnvironment
+  p <- getPool e
+  return Config
+    { environment = e
+    , pool = p
+    }
+
+getEnvironment :: IO Environment
+getEnvironment = do
+  m <- lookupEnv "SCOTTY_ENV"
+  let e = case m of
+        Nothing -> Development
+        Just s -> read s
+  return e
+
+getPool :: Environment -> IO DB.ConnectionPool
+getPool e = do
+  s <- getConnectionString e
+  let n = getConnectionSize e
+  case e of
+    Development -> runStdoutLoggingT (DB.createPostgresqlPool s n)
+    Production -> runStdoutLoggingT (DB.createPostgresqlPool s n)
+    Test -> runNoLoggingT (DB.createPostgresqlPool s n)
+
+getConnectionString :: Environment -> IO DB.ConnectionString
+getConnectionString e = do
+  m <- lookupEnv "DATABASE_URL"
+  let s = case m of
+        Nothing -> getDefaultConnectionString e
+        Just u -> createConnectionString (parseDatabaseUrl u)
+  return s
+
+getDefaultConnectionString :: Environment -> DB.ConnectionString
+getDefaultConnectionString Development =
+  "host=localhost port=5432 user=postgres dbname=event_calendar_development"
+getDefaultConnectionString Production =
+  "host=localhost port=5432 user=postgres dbname=event_calendar_production"
+getDefaultConnectionString Test =
+  "host=localhost port=5432 user=postgres dbname=event_calendar_test"
+
+createConnectionString :: [(T.Text, T.Text)] -> DB.ConnectionString
+createConnectionString l =
+  let f (k, v) = T.concat [k, "=", v]
+  in  encodeUtf8 (T.unwords (map f l))
+
+getConnectionSize :: Environment -> Int
+getConnectionSize Development = 1
+getConnectionSize Production = 8
+getConnectionSize Test = 1
+
+getOptions :: Environment -> IO Options
+getOptions e = do
+  s <- getSettings e
+  return def
+    { settings = s
+    , verbose = case e of
+      Development -> 1
+      Production -> 0
+      Test -> 0
+    }
+
+getSettings :: Environment -> IO Settings
+getSettings e = do
+  let s = defaultSettings
+
+      s' = case e of
+        Development -> setFdCacheDuration 0 s
+        Production -> s
+        Test -> s
+  m <- getPort
+  let s'' = case m of
+        Nothing -> s'
+        Just p -> setPort p s'
+  return s''
+
+getPort :: IO (Maybe Int)
+getPort = do
+  m <- lookupEnv "PORT"
+  let p = case m of
+        Nothing -> Nothing
+        Just s -> Just (read s)
+  return p
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,100 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Main where
-import Control.Applicative (Applicative)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Logger (runNoLoggingT, runStdoutLoggingT)
 import Control.Monad.Reader (MonadReader, ReaderT, asks, runReaderT)
 import Control.Monad.Trans.Class (MonadTrans, lift)
 import Data.Aeson (Value (Null), (.=), object)
-import Data.Default (def)
-import qualified Data.Text as T
-import Data.Text.Encoding (encodeUtf8)
 import Data.Text.Lazy (Text)
 import qualified Database.Persist as DB
 import qualified Database.Persist.Postgresql as DB
-import Model (Application, ApplicationId, Event, EventId, migrateAll)
 import Network.HTTP.Types.Status (created201, internalServerError500,
   notFound404)
 import Network.Wai (Middleware)
-import Network.Wai.Handler.Warp (Settings, defaultSettings,
-  setFdCacheDuration, setPort)
 import Network.Wai.Middleware.RequestLogger (logStdout, logStdoutDev)
-import System.Environment (lookupEnv)
-import Web.Heroku (parseDatabaseUrl)
 import Web.Scotty.Trans (ActionT, Options, ScottyT, defaultHandler, delete,
   get, json, jsonData, middleware, notFound, param, post, put, scottyOptsT,
-  settings, showError, status, verbose)
+  settings, showError, status)
+
+import Config
+import Model (Application, ApplicationId, Event, EventId, migrateAll)
+
+type Error = Text
 
 main :: IO ()
-main = do
-  c <- getConfig
-  runApplication c
-
-getConfig :: IO Config
-getConfig = do
-  e <- getEnvironment
-  p <- getPool e
-  return Config
-    { environment = e
-    , pool = p
-    }
-
-data Config = Config
-  { environment :: Environment
-  , pool :: DB.ConnectionPool
-  }
-
-getEnvironment :: IO Environment
-getEnvironment = do
-  m <- lookupEnv "SCOTTY_ENV"
-  let e = case m of
-        Nothing -> Development
-        Just s -> read s
-  return e
-
-data Environment
-  = Development
-  | Production
-  | Test
-  deriving (Eq, Read, Show)
-
-getPool :: Environment -> IO DB.ConnectionPool
-getPool e = do
-  s <- getConnectionString e
-  let n = getConnectionSize e
-  case e of
-    Development -> runStdoutLoggingT (DB.createPostgresqlPool s n)
-    Production -> runStdoutLoggingT (DB.createPostgresqlPool s n)
-    Test -> runNoLoggingT (DB.createPostgresqlPool s n)
-
-getConnectionString :: Environment -> IO DB.ConnectionString
-getConnectionString e = do
-  m <- lookupEnv "DATABASE_URL"
-  let s = case m of
-        Nothing -> getDefaultConnectionString e
-        Just u -> createConnectionString (parseDatabaseUrl u)
-  return s
-
-getDefaultConnectionString :: Environment -> DB.ConnectionString
-getDefaultConnectionString Development =
-  "host=localhost port=5432 user=postgres dbname=event_calendar_development"
-getDefaultConnectionString Production =
-  "host=localhost port=5432 user=postgres dbname=event_calendar_production"
-getDefaultConnectionString Test =
-  "host=localhost port=5432 user=postgres dbname=event_calendar_test"
-
-createConnectionString :: [(T.Text, T.Text)] -> DB.ConnectionString
-createConnectionString l =
-  let f (k, v) = T.concat [k, "=", v]
-  in  encodeUtf8 (T.unwords (map f l))
-
-getConnectionSize :: Environment -> Int
-getConnectionSize Development = 1
-getConnectionSize Production = 8
-getConnectionSize Test = 1
+main = getConfig >>= runApplication
 
 runApplication :: Config -> IO ()
 runApplication c = do
@@ -102,46 +31,7 @@ runApplication c = do
 
   let r m = runReaderT (runConfigM m) c
   scottyOptsT o r r application
-
-newtype ConfigM a = ConfigM
- { runConfigM :: ReaderT Config IO a
- } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
-
-getOptions :: Environment -> IO Options
-getOptions e = do
-  s <- getSettings e
-  return def
-    { settings = s
-    , verbose = case e of
-      Development -> 1
-      Production -> 0
-      Test -> 0
-    }
-
-getSettings :: Environment -> IO Settings
-getSettings e = do
-  let s = defaultSettings
-
-      s' = case e of
-        Development -> setFdCacheDuration 0 s
-        Production -> s
-        Test -> s
-  m <- getPort
-  let s'' = case m of
-        Nothing -> s'
-        Just p -> setPort p s'
-  return s''
-
-getPort :: IO (Maybe Int)
-getPort = do
-  m <- lookupEnv "PORT"
-  let p = case m of
-        Nothing -> Nothing
-        Just s -> Just (read s)
-  return p
-
-type Error = Text
-
+  
 application :: ScottyT Error ConfigM ()
 application = do
   runDB (DB.runMigration migrateAll)
